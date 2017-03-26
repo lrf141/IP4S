2fff3a969f4be40426250601da64234996d41d9e
lrf141 <ghost141.kentyo@gmail.com>
2017-03-27 00:07:48 +0900

    junit version up and passed unit test

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object MyBuild extends Build{
             scalaVersion := "2.12.0",
             libraryDependencies ++= Seq(
                 "org.scalatest" %% "scalatest" % "3.0.1" % "test",
-                "junit" % "junit" % "4.11" % "test"
+                "junit" % "junit" % "4.12" % "test"
             )
         )
     )
