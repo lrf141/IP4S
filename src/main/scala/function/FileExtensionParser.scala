c286967654b1c68f89d2407f89c1510981e5809b
lrf141 <ghost141.kentyo@gmail.com>
2017-04-11 22:28:21 +0900

    add FileExtensionParser.scala

---
+++ src/main/scala/function/FileExtensionParser.scala
@@ -0,0 +1,10 @@
+/*
+*  IP4S/src/main/scala/function/FileExtensionParser.scala
+* 
+*  Copyright (C) 2017 K.Takeuchi
+*
+*  A class that implements a function for determining the file extension
+*/
+
+object FileExtensionParser{
+}
